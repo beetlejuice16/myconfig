56059cc1cebf49e65b896cba690646c3a9ece31a
Fayez <fayez.bajjali@proton.me>
2024-01-25 15:20:42 +0100

    Adjust the init file

--- .config/nvim/lua/beetle/plugins/init.lua
+++ .config/nvim/lua/beetle/plugins/init.lua
@@ -1,11 +1,8 @@
 return {
-    {
-        'mbbill/undotree',
-        config = function()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-        end
-    },
     'tpope/vim-surround',
     'christoomey/vim-tmux-navigator',
+    { "numToStr/Comment.nvim", config = true },
     { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {} },
+    -- for working with the neovim dotfiles
+    { "folke/neodev.nvim",     opts = {},             priority = 999 },
 }
