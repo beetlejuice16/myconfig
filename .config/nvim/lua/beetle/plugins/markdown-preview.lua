e6298adbf98343fe17dbc3185ffd4cd08271880e
Fayez Bajjali <fayez.bajjali@regnology.net>
2024-03-13 16:54:09 +0100

    Rebind i3 workspaces and smaller changes

--- .config/nvim/lua/beetle/plugins/markdown-preview.lua
+++ .config/nvim/lua/beetle/plugins/markdown-preview.lua
@@ -2,5 +2,8 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+        vim.g.mkdp_auto_close = 0
+        vim.fn["mkdp#util#install"]()
+    end,
 }
