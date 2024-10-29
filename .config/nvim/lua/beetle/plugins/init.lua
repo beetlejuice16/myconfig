dc9b3b972564da0fe67c3ceac5a86aa3bd6e69cc
Fayez Bajjali <fayez.bajjali@regnology.net>
2024-10-29 09:10:38 +0100

    Remove duplicate entry present in other file

--- .config/nvim/lua/beetle/plugins/init.lua
+++ .config/nvim/lua/beetle/plugins/init.lua
@@ -2,7 +2,6 @@ return {
     'tpope/vim-surround',
     'christoomey/vim-tmux-navigator',
     { 'lewis6991/gitsigns.nvim', config = true },
-    { "numToStr/Comment.nvim",   config = true },
     { 'windwp/nvim-autopairs',   event = "InsertEnter", opts = {} },
     -- for working with the neovim dotfiles
     { "folke/neodev.nvim",       opts = {},             priority = 999 },
